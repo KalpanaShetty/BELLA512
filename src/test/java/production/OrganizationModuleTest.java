58d6e4d1573794b2e791882922753acc97bd12a4
KalpanaShetty <kalpanashetty512@gmail.com>
2022-07-14 16:30:07 +0530

    first commit

---
+++ src/test/java/production/OrganizationModuleTest.java
@@ -0,0 +1,15 @@
+package production;
+
+import org.testng.annotations.Test;
+
+public class OrganizationModuleTest {
+	@Test
+	public void createContactTest() {
+		System.out.println("createContactTest executed");
+	}
+	@Test
+	public void createOrganizationTest() {
+		System.out.println("createOrganizationTest executed");
+	}
+		}
+
